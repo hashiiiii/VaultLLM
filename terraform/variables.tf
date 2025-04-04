50b0a088f2395e664d0e6c75acb5d149ba056b94
hashiiiii <pkj59wwj@gmail.com>
2025-04-04 23:27:02 +0900

    refactor(ecs): Use variables, remote state, and update resources

--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "ap-northeast-1" # Tokyo
-}
-
-# 後ほど Availability Zone など他の変数も追加します 
